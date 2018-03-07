0a093a2e41ce8a54b92f42b7c07143dfd4e54cb8
loizes <33106820+loizes@users.noreply.github.com>
2018-03-07 08:30:09 +0300

    1
    i nedd more sleep

--- js/randomgallery.js
+++ js/randomgallery.js
@@ -1,7 +1,61 @@
-$(document).ready(function(){
-$(".thumbnail").click(function(e){
-    e.preventDefault();
-})
+// var imgs = [ '../img/inst/1.jpg', '../img/inst/2.jpg', '../img/inst/3.jpg', '../img/inst/4.jpg','../img/inst/5.jpg'];
+// setInterval(function(){
+//     img = imgs[Math.floor(Math.random()*imgs.length)];
 
+//     $('.tmbpicgallery1').attr('src', img);
+// }, 3000);
+// setInterval(function(){
+//     img = imgs[Math.floor(Math.random()*imgs.length)];
 
-})
+//     $('.tmbpicgallery2').attr('src', img);
+// }, 3000);
+// setInterval(function(){
+//     img = imgs[Math.floor(Math.random()*imgs.length)];
+
+//     $('.tmbpicgallery3').attr('src', img);
+// }, 3000);
+// setInterval(function(){
+//     img = imgs[Math.floor(Math.random()*imgs.length)];
+
+//     $('.tmbpicgallery4').attr('src', img);
+// }, 3000);
+// setInterval(function(){
+//     img = imgs[Math.floor(Math.random()*imgs.length)];
+
+//     $('.tmbpicgallery5').attr('src', img);
+// }, 3000);
+// setInterval(function(){
+//     img = imgs[Math.floor(Math.random()*imgs.length)];
+
+//     $('.tmbpicgallery6').attr('src', img);
+// }, 3000);
+// setInterval(function(){
+//     img = imgs[Math.floor(Math.random()*imgs.length)];
+
+//     $('.tmbpicgallery7').attr('src', img);
+// }, 3000);
+// setInterval(function(){
+//     img = imgs[Math.floor(Math.random()*imgs.length)];
+
+//     $('.tmbpicgallery8').attr('src', img);
+// }, 3000);
+// setInterval(function(){
+//     img = imgs[Math.floor(Math.random()*imgs.length)];
+
+//     $('.tmbpicgallery9').attr('src', img);
+// }, 3000);
+// setInterval(function(){
+//     img = imgs[Math.floor(Math.random()*imgs.length)];
+
+//     $('.tmbpicgallery10').attr('src', img);
+// }, 3000);
+// setInterval(function(){
+//     img = imgs[Math.floor(Math.random()*imgs.length)];
+
+//     $('.tmbpicgallery11').attr('src', img);
+// }, 3000);
+// setInterval(function(){
+//     img = imgs[Math.floor(Math.random()*imgs.length)];
+
+//     $('.tmbpicgallery12').attr('src', img);
+// }, 3000);
